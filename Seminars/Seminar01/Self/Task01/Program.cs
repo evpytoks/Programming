b35a53f0616b03628f53b4bfaffb1007daf44cc4
evpytoks <kuznetdaria@gmail.com>
2023-09-10 18:27:22 +0300

    done task1

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,12 @@
-﻿// i ve started work
+﻿using System;
+class Program
+{
+    static void Main()
+    {   string surname = Console.ReadLine();
+        string name = Console.ReadLine();
+        string second_name = Console.ReadLine();
+        Console.WriteLine($"Фамилия: {surname}");
+        Console.WriteLine($"Имя: {name}");
+        Console.WriteLine($"Отчество: {second_name}");
+    }
+}
