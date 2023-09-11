542424e23c107b379a5b1ad1844191082541202b
evpytoks <kuznetdaria@gmail.com>
2023-09-11 15:34:40 +0300

    done task04

--- Seminars/Seminar01/Self/Task04/Program.cs
+++ Seminars/Seminar01/Self/Task04/Program.cs
@@ -1,1 +1,12 @@
-﻿// Your code here!
+﻿using System;
+class Program
+{
+    static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear(); // изменён цвет фона
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Ура, я цветная");   
+        }
+}
