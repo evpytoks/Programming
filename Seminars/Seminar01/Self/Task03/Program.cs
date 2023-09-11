0a1b0c74c1554d5eb6da36275f4f6d3522e97780
evpytoks <kuznetdaria@gmail.com>
2023-09-11 13:31:09 +0300

    done task03

--- Seminars/Seminar01/Self/Task03/Program.cs
+++ Seminars/Seminar01/Self/Task03/Program.cs
@@ -1,1 +1,10 @@
-﻿// Your code here!
+﻿using System;
+class Program 
+{
+    static void Main() 
+    {
+        Console.WriteLine("Как тебя зовут?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}!");
+    }
+}
