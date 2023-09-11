b5030a474c6f5b9967f246078f693e916833cc3e
evpytoks <kuznetdaria@gmail.com>
2023-09-11 12:28:11 +0300

    Done task02

--- Seminars/Seminar01/Self/Task02/Program.cs
+++ Seminars/Seminar01/Self/Task02/Program.cs
@@ -1,1 +1,11 @@
-﻿// Your code here!
+﻿using System;
+class Program 
+{
+    static void Main()
+    {
+        string surname = Console.ReadLine();
+        string name = Console.ReadLine();
+        string second_name = Console.ReadLine();
+        Console.WriteLine($"Фамилия Имя Отчество: {surname} {name} {second_name}");
+    }
+}
