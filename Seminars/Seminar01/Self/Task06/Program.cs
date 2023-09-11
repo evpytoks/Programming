6b17f52ffe9b491c24b5628800ebbcfbb32b58be
evpytoks <kuznetdaria@gmail.com>
2023-09-11 16:21:09 +0300

    done task06

--- Seminars/Seminar01/Self/Task06/Program.cs
+++ Seminars/Seminar01/Self/Task06/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text.Unicode;
+class Proram {
+    static void Main()
+    {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        bool f1, f2;
+        double a_num, b_num, c;
+        f1 = double.TryParse(a, out a_num);
+        f2 = double.TryParse(b, out b_num);
+        if (f1 && f2)  // проверка на то, что a и b числа.
+        {
+            c = Math.Sqrt(a_num * a_num + b_num * b_num);
+            Console.WriteLine($"Гипотинуза: {c}");
+        } 
+        else 
+        {
+            Console.WriteLine("Ошибка");
+        }
+    }
+}
