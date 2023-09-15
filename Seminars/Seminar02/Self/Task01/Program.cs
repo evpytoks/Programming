5e09cab425789c910d61e1c0704444ffe9c75c28
evpytoks <kuznetdaria@gmail.com>
2023-09-15 19:40:38 +0300

    Done task01

---
+++ Seminars/Seminar02/Self/Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program 
+{
+    static void Main() 
+    {
+        int num1 = 1;
+        double num2 = 0.5;
+        char symble = '1';
+        Console.WriteLine($"{num1}");
+        Console.WriteLine($"{num2}");
+        Console.WriteLine($"{symble}");
+        Console.WriteLine(num1.GetType());
+        Console.WriteLine(num2.GetType());
+        Console.WriteLine(symble.GetType());
+    }
+}
