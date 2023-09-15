f1ea6cb74af2992b4174004cb047f99c3ac983a2
evpytoks <kuznetdaria@gmail.com>
2023-09-15 23:41:24 +0300

    done task06

---
+++ Seminars/Seminar02/Self/Task06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main() 
+    {
+        Console.WriteLine("Введите целое трёхзначное число:");
+        string num_str = Console.ReadLine();
+        int num;
+        bool f = int.TryParse(num_str, out num);
+        if (f && ((num_str.Length == 3 && num_str[0] != '-') || (num_str.Length == 4 && num_str[0] == '-'))) //Проверка на корректность ввода
+        {
+            if (num_str.Length == 3 && num_str[0] != '-')
+            {
+                Console.WriteLine(num_str[0]);
+                Console.WriteLine(num_str[1]);
+                Console.WriteLine(num_str[2]);
+            }
+            else 
+            {
+                Console.WriteLine(num_str[1]);
+                Console.WriteLine(num_str[2]);
+                Console.WriteLine(num_str[3]);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка");
+        }
+    }
+}
