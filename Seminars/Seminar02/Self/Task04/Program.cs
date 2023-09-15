284b705ee82ab2c330b19eb4eaf87c606b54066d
evpytoks <kuznetdaria@gmail.com>
2023-09-15 22:23:03 +0300

    done task04

---
+++ Seminars/Seminar02/Self/Task04/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string sym = Console.ReadLine();
+        string ans = Char.IsDigit(sym[0]) ? "Это цифра" : (Char.IsLetter(sym[0]) ? "Это буква" : "Это ни буква, ни цифра");
+        Console.WriteLine(ans);
+    }
+}
