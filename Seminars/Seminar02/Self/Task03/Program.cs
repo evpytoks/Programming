0efe30795c3c70afa789efa08cf92b6acb0b1b7f
evpytoks <kuznetdaria@gmail.com>
2023-09-15 20:52:00 +0300

    done task03

---
+++ Seminars/Seminar02/Self/Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string str_x = Console.ReadLine();
+        string str_y = Console.ReadLine();
+        string str_z = Console.ReadLine();
+        int x = int.Parse(str_x);
+        int y = int.Parse(str_y);
+        int z = int.Parse(str_z);
+        int small = x <= y ? (x <= z ? x : z) : (y <= z ? y : z);
+        int big = x >= y ? (x >= z ? x : z) : (y >= z ? y : z);
+        int medium = x + y + z - small - big;
+        Console.WriteLine($"{small}");
+        Console.WriteLine($"{medium}");
+        Console.WriteLine($"{big}");
+    }
+}
