f3cfae6f29348d52ff9619f03f9dcdb6d87fc233
evpytoks <kuznetdaria@gmail.com>
2023-09-19 19:59:25 +0300

    done task01

---
+++ Seminars/Seminar03/Self/Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число углов (от 1 до 10) :");
+        string n_str = Console.ReadLine();
+        Console.WriteLine("Введите длину стороны (положительную) :");
+        string l_str = Console.ReadLine();
+        double n;
+        double l;
+        bool f1 = double.TryParse(n_str, out n);
+        bool f2 = double.TryParse(l_str, out l);
+        if (f1 && f2)
+        {
+            double s = (n * l * l) / (4.0 * Math.Tan(Math.PI / n));
+            Console.WriteLine(s);
+        }
+        else 
+        {
+            Console.WriteLine("Ошибка");
+        }
+    }
+}
