719ebfeef48f4bda5499da0dd3c93b7c51242f3b
evpytoks <kuznetdaria@gmail.com>
2023-09-20 16:54:13 +0300

    done task01

---
+++ Seminars/Seminar04/Self/Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string symbol = Console.ReadLine();
+        switch(symbol[0])
+        {
+            case not 'A' when Char.IsDigit(symbol[0]):
+                Console.WriteLine("100");
+                break;
+            case not '1' when Char.IsLetter(symbol[0]) && Char.IsUpper(symbol[0]):
+                Console.WriteLine("200");
+                break;
+            case not '1' when Char.IsLetter(symbol[0]) && Char.IsLower(symbol[0]):
+                Console.WriteLine("300");
+                break;
+            default:
+                Console.WriteLine("400");
+                break;
+        }
+    }
+}
