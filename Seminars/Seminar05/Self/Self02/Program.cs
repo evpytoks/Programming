e41ef1fcdf406750920314c91516a383bcf1d58c
evpytoks <kuznetdaria@gmail.com>
2023-09-25 20:37:24 +0300

    done task02 in seminar05

--- Seminars/Seminar05/Self/Self02/Program.cs
+++ Seminars/Seminar05/Self/Self02/Program.cs
@@ -1,1 +1,52 @@
-﻿// Ваше решение.
+﻿using System;
+class Program 
+{
+    static void Main()
+    {
+        int num_of_int = 0;
+        int num_of_double = 0;
+        int num_of_not_num = 0;
+        string input;
+        // Основной цикл ввода.
+        do
+        {
+            input = Console.ReadLine();
+            int num;
+            double num2;
+            // Проверка на тип введенных данных.
+            bool f;
+            f = double.TryParse(input, out num2);
+            if (f)
+            {
+                ++num_of_double;
+                f = int.TryParse(input, out num);
+                if (f)
+                {
+                    ++num_of_int;
+                }
+            } else
+            {
+                // Проверка на содержание цифр в строке.
+                int ind = 0;
+                bool f2 = true;
+                while (ind < input.Length)
+                {
+                    if (char.IsDigit(input[ind]))
+                    {
+                        f2 = false;
+                        break;
+                    }
+                    ++ind;
+                }
+                if (f2)
+                {
+                    ++num_of_not_num;
+                }
+            }
+        }
+        while (input != "0");
+        Console.WriteLine($"{num_of_int} - количество введённых целых чисел, удовлетворяющих типу int");
+        Console.WriteLine($"{num_of_double} -  количество введённых вещественных чисел, удовлетворяющих типу double");
+        Console.WriteLine($"{num_of_not_num} -  количество введённых нечисловых значений");
+    }
+}
