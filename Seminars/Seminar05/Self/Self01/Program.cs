9b696548d74007d1136e33cb1f3afacacee3a789
evpytoks <kuznetdaria@gmail.com>
2023-09-25 18:37:56 +0300

    done task01 from seminar05

--- Seminars/Seminar05/Self/Self01/Program.cs
+++ Seminars/Seminar05/Self/Self01/Program.cs
@@ -1,1 +1,25 @@
-﻿// Ваше решение.
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int number = 0;
+        while (number <= 50)
+        {
+            if (number == 20)
+            {
+                ++number;
+                continue;
+            }
+            if (number % 2 == 0 && number % 3 == 0)
+            {
+                Console.WriteLine($"Число {number} кратно 2 и 3");
+            } 
+            else
+            {
+                Console.WriteLine($"Число {number} не соответсвует условию");
+            }
+            ++number;
+        }
+    }
+}
