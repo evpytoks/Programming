9745031abfd0e1b7dc8b37edc3e9b2e8c980a393
evpytoks <kuznetdaria@gmail.com>
2023-09-27 15:22:21 +0300

    done task04 from seminar05

--- Seminars/Seminar05/Self/Self04/Program.cs
+++ Seminars/Seminar05/Self/Self04/Program.cs
@@ -1,1 +1,16 @@
-﻿// Ваше решение.
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        decimal summ = 0;
+        int n = 1;
+        // Считаем сумму.
+        while (n < 1000000)
+        {
+            summ += 1 / ((decimal)(n * (n + 1) * (n + 2)));
+            ++n;
+        }
+        Console.WriteLine(summ);
+    }
+}
