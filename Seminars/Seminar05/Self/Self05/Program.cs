7e4f9d2ce3b0d195c027ddaf8ffea70b4e24ff34
evpytoks <kuznetdaria@gmail.com>
2023-09-28 12:20:35 +0300

    done task5 in seminar5

--- Seminars/Seminar05/Self/Self05/Program.cs
+++ Seminars/Seminar05/Self/Self05/Program.cs
@@ -1,1 +1,27 @@
-﻿// Ваше решение.
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите натуральное числo k:");
+        string k_str;
+        k_str = Console.ReadLine();
+        // Перевод числа в нужный тип и проверка на корректность данных.
+        int k;
+        bool f = int.TryParse(k_str, out k);
+        if (f)
+        {
+            // Считаем сумму.
+            double summ = 0;
+            for (int i = 1; i < k + 1; ++i) 
+            {
+                summ += 1 / ((double)i);
+            }
+            Console.WriteLine(summ);
+        }
+        else 
+        {
+            Console.WriteLine("Ошибка");
+        }
+    }
+}
