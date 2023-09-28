47a425a20b5cf45fb543258c9868adaa989d6a4a
evpytoks <kuznetdaria@gmail.com>
2023-09-28 22:07:22 +0300

    done task1 from seminar6

--- Seminars/Seminar06/Self/Self01/Program.cs
+++ Seminars/Seminar06/Self/Self01/Program.cs
@@ -1,1 +1,23 @@
-﻿// Ваше решение.
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int p = -1, q = -1;
+        Console.WriteLine("p q f");
+        // Изменение p.
+        do
+        {
+        ++p;
+        // Изменение q.
+        do 
+        {
+            ++q;
+            int f = (((p & q) + 1) % 2) & (((p | ((q + 1) % 2)) + 1) % 2);
+            Console.WriteLine($"{p} {q} {f}");
+        } while (q != 1);
+        q = -1;
+        } while (p != 1);
+        
+    }
+}
