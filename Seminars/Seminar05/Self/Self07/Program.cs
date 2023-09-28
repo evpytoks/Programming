30e04be0f9d2b99343df4e2aade28a2d736be2c8
evpytoks <kuznetdaria@gmail.com>
2023-09-28 19:34:06 +0300

    done task7 from seminar5

--- Seminars/Seminar05/Self/Self07/Program.cs
+++ Seminars/Seminar05/Self/Self07/Program.cs
@@ -1,1 +1,26 @@
-﻿// Ваше решение.
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите натуральное число:");
+        string num = Console.ReadLine();
+        string answer = "";
+        // Переворот числа.
+        for (int i = 0; i < num.Length; ++i) 
+        {   
+            answer = num[i] + answer;
+        }
+        // Находим первый не ноль.
+        int ind = 0;
+        while (answer[ind] == '0')
+        {
+            ++ind;
+        }
+        // Вывод ответа.
+        for (int i = ind; i < answer.Length; ++i)
+        {
+            Console.Write(answer[i]);
+        }
+    }
+}
