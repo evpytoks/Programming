c5cb188f2a7779228191cdc259c4e91e7ff99cdc
evpytoks <kuznetdaria@gmail.com>
2023-09-28 21:04:14 +0300

    done task8 from seminar5

--- Seminars/Seminar05/Self/Self08/Program.cs
+++ Seminars/Seminar05/Self/Self08/Program.cs
@@ -1,1 +1,26 @@
-﻿// Ваше решение.
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите натуральное число:");
+        string num = Console.ReadLine();
+        Console.WriteLine("Введите количество разрядов, которые хотите вывести:");
+        string str_k = Console.ReadLine();
+        int k;
+        bool f = int.TryParse(str_k, out k);
+        // Проверка на корректность k.
+        if (f && k <= num.Length)
+        {
+            // Проходимся по k старшим разрядам.
+            for (int i = 0; i < k; ++i) {
+                Console.Write(num[i]);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка.");
+        }
+        
+    }
+}
