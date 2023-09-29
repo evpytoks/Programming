413c57fa453815f036003b34a353aafefe0edc03
evpytoks <kuznetdaria@gmail.com>
2023-09-29 18:55:42 +0300

    done task3 from seminar6

--- Seminars/Seminar06/Self/Self03/Program.cs
+++ Seminars/Seminar06/Self/Self03/Program.cs
@@ -1,1 +1,31 @@
-﻿// Ваше решение.
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int cnt = 0;
+        // Перебираем отрезок.
+        for (int i = 2000; i <= 3000; ++i)
+        {
+            string str_num = i.ToString();
+            bool f = true;
+            // Проверка на то, что цифры попарно различны.
+            for (int ind1  = 0; ind1 < 4; ++ind1)
+            {
+                for (int ind2 = ind1 + 1; ind2 < 4; ++ind2)
+                {
+                    if (str_num[ind1] == str_num[ind2])
+                    {
+                        f = false;
+                    }
+                }
+            }
+            if (f)
+            {
+                Console.WriteLine(str_num);
+                ++cnt;
+            }
+        }
+        Console.WriteLine(cnt);
+    }
+}
