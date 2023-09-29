a2b484fa4a4a1f2bcde3f56da4a4a9440c699a41
evpytoks <kuznetdaria@gmail.com>
2023-09-29 14:23:46 +0300

    done task2 from seminar6

--- Seminars/Seminar06/Self/Self02/Program.cs
+++ Seminars/Seminar06/Self/Self02/Program.cs
@@ -1,1 +1,22 @@
-﻿// Ваше решение.
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("A B C F");
+        // Перебираем А.
+        for (int a = 0; a < 2; ++a)
+        {
+            // Перебираем B.
+            for (int b = 0; b < 2; ++b)
+            {
+                // Перебираем С.
+                for (int c = 0; c < 2; ++c)
+                {
+                    // Выводим строку таблицы.
+                    Console.WriteLine($"{a} {b} {c} {((a | (b & c) + 1) % 2) | a}");
+                }
+            }
+        }
+    }
+}
